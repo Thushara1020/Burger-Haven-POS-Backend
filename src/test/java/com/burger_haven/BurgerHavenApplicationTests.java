6e85b0d2dc8053a59d9e876784cefeef2eabfb7c
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-09 20:23:37 +0530

    add initial Spring Boot context load test

---
+++ src/test/java/com/burger_haven/BurgerHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burger_haven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
