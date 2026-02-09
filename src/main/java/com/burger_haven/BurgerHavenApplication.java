bf22e1c5367744e93319105ca9db3315029d21a0
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-09 20:19:33 +0530

    add main Spring Boot application entry point

---
+++ src/main/java/com/burger_haven/BurgerHavenApplication.java
@@ -0,0 +1,13 @@
+package com.burger_haven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerHavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerHavenApplication.class, args);
+	}
+
+}
