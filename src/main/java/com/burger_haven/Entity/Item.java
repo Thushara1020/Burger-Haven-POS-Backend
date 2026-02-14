0f28a0addb5d27a236ac861eaa1213b40be45ecb
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-14 22:52:39 +0530

    add Spring scheduling import to entity

--- src/main/java/com/burger_haven/Entity/Item.java
+++ src/main/java/com/burger_haven/Entity/Item.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.scheduling.annotation.Schedules;
 
 @Entity
 @Data
